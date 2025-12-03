241d1b9fd07e7804e29c01280dc9975da1380318
Avi <gargavi2006@outlook.com>
2025-12-03 08:18:11 +0530

    Update Footer.jsx
    Remove credits

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
           <div className="h-1 w-20 mx-auto mb-4 bg-gradient-to-r from-blue-500 to-teal-400 rounded-full"></div>
   
           <p className="text-sm text-gray-500">
-            © {new Date().getFullYear()} Tiny Malaria Scan — Built with ❤️ by Avi
+            © {new Date().getFullYear()} Tiny Malaria Scan — Built by Group 10
           </p>
         </div>
       </footer>
